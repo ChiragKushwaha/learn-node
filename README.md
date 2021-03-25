03a5c217edee6406bbf51581c58dbb55c69a28fb
Chirag <chirag.kushwaha@314ecorp.com>
2021-03-25 12:37:15 +0530

    mongo validation completed

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # node
+
+"name" -inurl:(htm|html|php|pls|txt) intitle:index.of “last modified” (MP4|MOV|WMV|FLV|AVI|AVCHD|WEBM|MKV)
+
+MP4|MOV|WMV|FLV|AVI|AVCHD|WEBM|MKV
+WEBM|MPG|MP2|MPEG|MPE|MPV|OGG|MP4|M4P|M4V|AVI|WMV|MOV|QT|FLV|SWF|AVCHD
--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -9,11 +9,47 @@ mongoose
   .catch((err) => console.log("Could not connect to MongoDb...", err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: {
+    type: String,
+    required: true,
+    minlength: 5,
+    maxlength: 255,
+    // match: /pattern/
+  },
+  category: {
+    type: String,
+    required: true,
+    enum: ["web", "mobile", "network"],
+    lowercase: true,
+    trim: true, //remove extra space
+  },
   author: String,
-  tags: [String],
+  tags: {
+    type: Array,
+    validate: {
+      isAsync: true,
+      validator: function (v, callback) {
+        setTimeout(() => {
+          const result = v && v.length > 0;
+          callback(result);
+        }, 4000);
+        // return v && v.length > 0;
+      },
+      message: "A course should have atleast one tag",
+    },
+  },
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
+  price: {
+    type: Number,
+    required: function () {
+      return this.isPublished;
+    },
+    min: 10,
+    max: 200,
+    get: (v) => Math.round(v), // when we read the property value from the database it is called
+    set: (v) => Math.round(v), // when we set the value it is called
+  },
 });
 
 //Model
@@ -27,8 +63,19 @@ async function createCourse() {
     isPublished: true,
   });
 
-  const result = await course.save();
-  console.log(result);
+  try {
+    // await course.validate();
+    //  course.validate((error)=>{
+    //    if(error){
+    //      console.log(error);
+    //    }
+    //  });
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    ex.errors.map((e) => console.log(ex.errors[e].message));
+    // console.log(ex);
+  }
 }
 
 // createCourse();
